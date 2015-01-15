24048652290e1b768138d8cfbab44710e435f40e
Si Jobling <simon.jobling@gmail.com>
2015-01-15 08:26:52 +0000

    Create train ticket restriction scaffold

---
+++ app/Controller/TrainTicketRestrictionsController.php
@@ -0,0 +1,15 @@
+<?php
+class TrainTicketRestrictionsController extends AppController {
+
+  var $name = 'TrainTicketRestrictions';
+  var $scaffold;
+
+	function beforeRender() {
+	  parent::beforeRender();
+	}
+
+	function beforeFilter() {
+	  parent::beforeFilter();
+	}
+
+}
---
+++ app/Model/TrainTicketRestriction.php
@@ -0,0 +1,5 @@
+<?php
+class TrainTicketRestriction extends AppModel {
+	var $name = 'TrainTicketRestriction';
+	var $order = array('name ASC');
+}
