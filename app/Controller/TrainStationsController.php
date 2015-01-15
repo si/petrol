f90a2281247c443180cbc1bdbf95cadb06aa2f43
Si Jobling <simon.jobling@gmail.com>
2015-01-15 08:26:27 +0000

    Create train station scaffold

---
+++ app/Controller/TrainStationsController.php
@@ -0,0 +1,15 @@
+<?php
+class TrainStationsController extends AppController {
+
+  var $name = 'TrainStations';
+  var $scaffold;
+
+	function beforeRender() {
+	  parent::beforeRender();
+	}
+
+	function beforeFilter() {
+	  parent::beforeFilter();
+	}
+
+}
---
+++ app/Model/TrainStation.php
@@ -0,0 +1,6 @@
+<?php
+class TrainStation extends AppModel {
+	var $name = 'TrainStation';
+	var $order = array('code ASC');
+
+}
