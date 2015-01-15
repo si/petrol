acf0e209134299a1879853593911e431475dad8a
Si Jobling <simon.jobling@gmail.com>
2015-01-15 08:27:18 +0000

    Create location scaffold (for car park)

---
+++ app/Controller/LocationsController.php
@@ -0,0 +1,17 @@
+<?php
+class LocationsController extends AppController {
+
+  var $name = 'Locations';
+  var $scaffold;
+
+	function beforeRender() {
+	  parent::beforeRender();
+	}
+
+	function beforeFilter() {
+	  parent::beforeFilter();
+	}
+
+
+
+}
