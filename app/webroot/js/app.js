bbd141a44eadb8ef48cacc8c52313479a370efcb
Si Jobling <simon.jobling@gmail.com>
2015-03-17 22:55:08 +0000

    Add chart to receipts grid showing PPL

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
 
-	 function roundNumber(num, dec) {
-  	 var result = Math.round(num*Math.pow(10,dec))/Math.pow(10,dec);
-  	 return result;
-   }
+	function roundNumber(num, dec) {
+		var result = Math.round(num*Math.pow(10,dec))/Math.pow(10,dec);
+		return result;
+	}
 
-	 function updatePPL(ev) {
-	   current = $(ev.currentTarget).attr('id');
-	   console.log(current);
-	   cost = $('#ReceiptCost').val();
-	   litres = $('#ReceiptLitres').val();
-	   ppl = $('#ReceiptPricePerLitre').val();
-
-	   if((current=='ReceiptCost' || current=='ReceiptLitres') && (cost>0 && litres>0)) {
-	     $('#ReceiptPricePerLitre').val(roundNumber(cost/litres,3));
-	   } else if((current=='ReceiptCost' || current=='ReceiptPricePerLitre') && (cost>0 && ppl>0)) {
-	     $('#ReceiptLitres').val(roundNumber(cost/ppl,2));
-	   }
+	function updatePPL(ev) {
+		current = $(ev.currentTarget).attr('id');
+		cost = $('#ReceiptCost').val();
+		litres = $('#ReceiptLitres').val();
+		ppl = $('#ReceiptPricePerLitre').val();
+		
+		if((current=='ReceiptCost' || current=='ReceiptLitres') && (cost>0 && litres>0)) {
+			$('#ReceiptPricePerLitre').val(roundNumber(cost/litres,3));
+		} else if((current=='ReceiptCost' || current=='ReceiptPricePerLitre') && (cost>0 && ppl>0)) {
+			$('#ReceiptLitres').val(roundNumber(cost/ppl,2));
+		}
 	 }
 
 	 $('#ReceiptCost, #ReceiptLitres, #ReceiptPricePerLitre').on('keydown', updatePPL);
@@ -181,4 +180,100 @@ $(document).ready(function() {
 function clear_cookies() {
     $.cookie('posLat', null);
     document.getElementById("status").innerHTML = "Cookies cleared.";
+}
+
+var receiptChart = (function receiptsChart() {
+
+	// Configure some options
+	var options = {
+	    scaleShowGridLines : true,
+	    scaleGridLineColor : "rgba(0,0,0,.05)",
+	    scaleGridLineWidth : 1,
+	    scaleShowHorizontalLines: true,
+	    scaleShowVerticalLines: false,
+	    bezierCurve : true,
+	    bezierCurveTension : 0.4,
+	    pointDot : true,
+	    pointDotRadius : 4,
+	    pointDotStrokeWidth : 1,
+	    pointHitDetectionRadius : 20,
+	    datasetStroke : true,
+	    datasetStrokeWidth : 2,
+	    datasetFill : true,
+	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+	};
+	
+	// Establish some data points
+	var $table = $('table.chart tr'),
+		chart_data = {
+			dates : [],
+			prices : [],
+			totals : []
+		};
+	
+	// Loop through data table
+	$($table).each(function(index, row) {
+
+		date = $('td:first a', row);
+		date_utc = date.data('short');
+		if(date_utc!==null) {
+		
+			chart_data.dates.push(date_utc);
+	
+			price = $('td:nth-child(3)', row);
+			price_float = convertPriceToFloat(price.text());
+			if(price_float!==0) chart_data.prices.push(price_float);
+	
+			total = $('td:nth-child(2)', row);
+			total_float = convertPriceToFloat(total.text());
+			if(total_float!==0) chart_data.totals.push(total_float);
+		}
+	});
+	
+	// Reverse array values
+	chart_data.dates.reverse();
+	chart_data.prices.reverse();
+	chart_data.totals.reverse();
+	
+	// Compile chart data
+	var data = {
+		    labels: chart_data.dates,
+		    datasets: [
+		        {
+		            label: "Price",
+		            fillColor: "rgba(220,220,220,0.2)",
+		            strokeColor: "rgba(220,220,220,1)",
+		            pointColor: "rgba(220,220,220,1)",
+		            pointStrokeColor: "#fff",
+		            pointHighlightFill: "#fff",
+		            pointHighlightStroke: "rgba(220,220,220,1)",
+		            data: chart_data.prices
+		        }
+/*
+		        ,
+		        {
+		            label: "Total",
+		            fillColor: "rgba(220,255,220,0.2)",
+		            strokeColor: "rgba(220,255,220,1)",
+		            pointColor: "rgba(220,255,220,1)",
+		            pointStrokeColor: "#cfc",
+		            pointHighlightFill: "#cfc",
+		            pointHighlightStroke: "rgba(220,255,220,1)",
+		            data: chart_data.totals
+		        }
+*/
+		    ]
+	};
+	
+
+
+	// Get context with jQuery - using jQuery's .get() method.
+	var ctx = $("#receiptsChart").get(0).getContext("2d");
+	// This will get the first returned node in the jQuery collection.
+	var myLineChart = new Chart(ctx).Line(data, options);
+	
+})();
+
+function convertPriceToFloat(string) {
+	return Number(string.replace(/[^0-9\.]+/g,""));
 }
