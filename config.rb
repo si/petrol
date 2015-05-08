d17b140eda66f4106908fa37524a2e27f33db5ec
Si Jobling <simon.jobling@gmail.com>
2015-05-08 17:27:26 +0100

    Move ruby config (for Compass) to the root

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "app/webroot/css"
+sass_dir = "app/webroot/sass"
+images_dir = "app/webroot/images"
+javascripts_dir = "app/webroot/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
