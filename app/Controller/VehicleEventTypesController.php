ea460f001c4b7c7304a42e7c100949393cb9dc36
Si Jobling <simon.jobling@gmail.com>
2015-05-18 07:36:12 +0100

    Create Vehicle Events Type model (e.g. insurance, breakdown renewal, tax, MOT, service) with basic scaffolding

---
+++ app/Controller/VehicleEventTypesController.php
@@ -0,0 +1,14 @@
+<?php
+class VehicleEventTypesController extends AppController {
+
+	var $name = 'VehicleEventTypes';
+	var $scaffold;
+
+	function beforeRender() {
+		parent::beforeRender();
+	}
+
+	function beforeFilter() {
+		parent::beforeFilter();
+	}
+}
---
+++ app/Model/VehicleEventType.php
@@ -0,0 +1,5 @@
+<?php
+class VehicleEventType extends AppModel {    
+	var $name = 'VehicleEventType';
+	var $displayField = 'name';
+}
