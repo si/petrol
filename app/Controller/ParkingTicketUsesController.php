5a65f4eddaaf84afcb0353907a108e2de89e0872
Si Jobling <simon.jobling@gmail.com>
2015-06-09 08:51:18 +0100

    Set parking ticket use defaults based on passed ticket ID (start and end date/times)

--- app/Controller/ParkingTicketUsesController.php
+++ app/Controller/ParkingTicketUsesController.php
@@ -26,10 +26,26 @@ class ParkingTicketUsesController extends AppController {
 
 		// Nothing posted
 	    } else {
+
+	    	$defaults = array();
+
+	    	// Set the parking ticket based on passed param
 			if(isset($this->params['named']['parking_ticket_id'])) {
+
 				$defaults['ParkingTicketUse']['parking_ticket_id'] = $this->params['named']['parking_ticket_id'];
-				$this->data = $defaults;
+
+				// Get the parking ticket data
+				$parkingTicket = $this->ParkingTicketUse->ParkingTicket->findById($this->params['named']['parking_ticket_id']);
+
+				// Set START date/time based on parking ticket start date
+				$defaults['ParkingTicketUse']['starts'] = $parkingTicket['ParkingTicket']['created'];
+				
+				// Set END date/time based on parking ticket end date
+				$defaults['ParkingTicketUse']['ends'] = $parkingTicket['ParkingTicket']['expires'];
+
 			}
+
+			$this->data = $defaults;
 		}
 
 		// Lookups
