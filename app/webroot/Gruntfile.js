ee06bbd1a552efbfa3baccd1419801cbb44aed1d
Si Jobling <simon.jobling@gmail.com>
2015-10-02 18:33:43 +0100

    Disable Grunt sass task

--- app/webroot/Gruntfile.js
+++ app/webroot/Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-image-embed");
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-	grunt.registerTask('default', ['concat', 'uglify', 'sass']);
-	grunt.registerTask('dev', ['concat', 'uglify', 'sass', 'watch']);
+	grunt.registerTask('default', ['concat', 'uglify']);
+	grunt.registerTask('dev', ['concat', 'uglify', 'watch']);
 
 };
